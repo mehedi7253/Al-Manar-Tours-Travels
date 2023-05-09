96039a789f30ae2208b4da65d88a3cb0a24c8dd1
Mehedi Hasan <46008075+mehedi7253@users.noreply.github.com>
2023-05-09 12:01:02 +0600

    Update db_connect.php

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,10 +1,10 @@
 <?php
 
 
-    $servername = "localhost";
-    $username   = "root";
-    $password   = "";
-    $dbname     = "Almanar";
+    $servername = "almanar-server.mysql.database.azure.com";
+    $username   = "xzepnnomko";
+    $password   = "855G5M5SOB6TMVQ0$";
+    $dbname     = "almanar-database";
 
     // crearte connection
     $connect = new Mysqli($servername, $username, $password, $dbname);
